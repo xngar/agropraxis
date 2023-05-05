a3b98bc03f8cf0b59ec82a143fcfef856bde3339
Mauricio Zuñiga <mantonio.zr@gmail.com>
2023-05-05 18:36:07 -0400

    ajustes al getcliente

--- src/componentes/NavBar.js
+++ src/componentes/NavBar.js
@@ -1,30 +1,46 @@
-import React from 'react'
-import {VscAccount} from "react-icons/vsc"
-import {MdMonitor} from "react-icons/md"
-import {GiGrapes} from "react-icons/gi"
-import "./NavBar.css"
+import React from "react";
+import { VscAccount } from "react-icons/vsc";
+import { MdMonitor } from "react-icons/md";
+import { GiGrapes } from "react-icons/gi";
+import "./NavBar.css";
 
-const NavBar = ({cliente}) => {
+const NavBar = ({ cliente }) => {
   const clientes = localStorage.getItem("cliente");
-  const status =  localStorage.getItem("status");
+  const status = localStorage.getItem("status");
+
   return (
-   <div>
-    
-    <div className='perfil'>
-    <img src="logo.png" width="100"/>
-      <h2>Perfil</h2>
-      <h5>Cliente: {cliente[0].RazonSocial}</h5>
-      <h5>Rut: {cliente[0].Rut}</h5>
-      <h5>Provincia:{cliente[0].NombreProvincia}</h5>
-      <h5>Comuna:{cliente[0].NombreComuna}</h5>
+    <div>
+      <div className="perfil">
+        <img src="logo.png" width="100" />
+        {cliente &&
+          cliente.map((mapeo) => {
+            return (
+              <div>
+                <h2>Perfil</h2>
+                <h5>Cliente: {mapeo.RazonSocial}</h5>
+                <h5>Rut: {mapeo.Rut}</h5>
+                <h5>Provincia:{mapeo.NombreProvincia}</h5>
+                <h5>Comuna:{mapeo.NombreComuna}</h5>
+              </div>
+            );
+          })}
+      </div>
+      <ul>
+        <li>
+          <span>
+            <MdMonitor />
+          </span>
+          Monitoreo
+        </li>
+        <li>
+          <span>
+            <GiGrapes />
+          </span>
+          Acidez de Frutos
+        </li>
+      </ul>
     </div>
-    <ul>
-        
-        <li><span><MdMonitor/></span>Monitoreo</li>
-        <li><span><GiGrapes/></span>Acidez de Frutos</li>
-    </ul>
-   </div>
-  )
-}
+  );
+};
 
 export default NavBar;
