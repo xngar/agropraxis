71100e44f7875f69602e3c48b6e7f4d5ff71deda
xngar <mantonio.zr@gmail.com>
2023-09-28 23:04:27 +0000

    Funcionalidad Lores

--- src/utilidades/constantes.js
+++ src/utilidades/constantes.js
@@ -1,3 +1,3 @@
-export const URL_API_AGP = "https://localhost:7126";  // https://localhost:7126
+//export const URL_API_AGP = "https://localhost:7126";  // https://localhost:7126
 export const URL_PATH_MEDIA_APG = "https://agropraxisgroup.cl/upload/";
-// export const URL_API_AGP = "https://service.agropraxisgroup.cl"; 
+ export const URL_API_AGP = "https://service.agropraxisgroup.cl"; 
